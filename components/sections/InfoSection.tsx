9f959cfc3e94c6fafaf37f74cdf34a9e116b0e4e
Trần Thanh Độ <trthanhdo41@gmail.com>
2025-11-04 22:49:15 +0700

    fix: use ES5 compatible regex for diacritics

--- components/sections/InfoSection.tsx
+++ components/sections/InfoSection.tsx
@@ -22,7 +22,7 @@ export default function InfoSection() {
     return s
       .toLowerCase()
       .normalize("NFD")
-      .replace(/\p{Diacritic}/gu, "")
+      .replace(/[\u0300-\u036f]/g, "")
       .replace(/[^a-z0-9\s-]/g, "")
       .trim()
       .replace(/\s+/g, "-");
